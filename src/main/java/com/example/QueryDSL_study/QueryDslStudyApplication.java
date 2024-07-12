ff2b89e9fbc47f5133740c39fe3f3f7bb6eb3104
pcochoco <cochocolatecoo@gmail.com>
2024-07-12 18:39:17 +0900

    project made

---
+++ src/main/java/com/example/QueryDSL_study/QueryDslStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.QueryDSL_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslStudyApplication.class, args);
+	}
+
+}
